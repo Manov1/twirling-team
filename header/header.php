7d7fdd9933e4a8651f34b882bbde29ba025fbeab
dva-mano <557518@edu.rocmn.nl>
2022-10-10 12:02:20 +0200

    Header

---
+++ header/header.php
@@ -0,0 +1,30 @@
+<header class="bg-header">
+  <nav class="nav-width" aria-label="Top">
+    <div class="nav-border">
+      <div class="logo-searchbar">
+        <a href="index.php">
+          <span class="sr-only">Twirling team sparkle logo</span>
+          <img class="logo" src="https://tailwindui.com/img/logos/mark.svg?color=white" alt="">
+        </a>
+        <div class="searchbar">
+        <div class="searchbar-width">
+          <label for="search" class="sr-only">Search</label>
+          <div class="icon-relative">
+            <div class="magnifying-glass">
+              <!-- Heroicon name: mini/magnifying-glass -->
+              <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clip-rule="evenodd" />
+              </svg>
+            </div>
+            <input id="search" name="search" class="search-input text-sm placeholder-gray-500 focus:border-indigo-500 focus:text-gray-900 focus:placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm" placeholder="Search" type="search">
+          </div>
+        </div>
+        </div>
+      </div>
+      <div class="ml-10 space-x-4">
+        <a href="#" class="inline-block rounded-md border border-transparent bg-indigo-500 py-2 px-4 text-base font-medium text-white hover:bg-opacity-75">Sign in</a>
+        <a href="#" class="inline-block rounded-md border border-transparent bg-white py-2 px-4 text-base font-medium text-indigo-600 hover:bg-indigo-50">Sign up</a>
+      </div>
+    </div>
+  </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Twirling team sparkle</title>
+
+    <link rel="stylesheet" href="header/header.css">
+</head>
+<body>
+<?php include 'header/header.php'; ?>
+</body>
+</html>
