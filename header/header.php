dccd45ab8ba9216d1b1af3675157e975e7e423e3
dva-mano <557518@edu.rocmn.nl>
2022-10-10 14:43:22 +0200

    header update

--- header/header.php
+++ header/header.php
@@ -4,7 +4,7 @@
       <div class="logo-searchbar">
         <a href="index.php">
           <span class="sr-only">Twirling team sparkle logo</span>
-          <img class="logo" src="https://tailwindui.com/img/logos/mark.svg?color=white" alt="">
+          <img class="logo" src="../images/sparkle_twirling.png" alt="">
         </a>
         <div class="searchbar">
         <div class="searchbar-width">
@@ -16,14 +16,14 @@
                 <path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clip-rule="evenodd" />
               </svg>
             </div>
-            <input id="search" name="search" class="search-input text-sm placeholder-gray-500 focus:border-indigo-500 focus:text-gray-900 focus:placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm" placeholder="Search" type="search">
+            <input id="search" name="search" class="search-input" placeholder="Search" type="search">
           </div>
         </div>
         </div>
       </div>
       <div class="ml-10 space-x-4">
-        <a href="#" class="inline-block rounded-md border border-transparent bg-indigo-500 py-2 px-4 text-base font-medium text-white hover:bg-opacity-75">Sign in</a>
-        <a href="#" class="inline-block rounded-md border border-transparent bg-white py-2 px-4 text-base font-medium text-indigo-600 hover:bg-indigo-50">Sign up</a>
+        <a href="#" class="header-button1 text-white hover:bg-opacity-75">Sign in</a>
+        <a href="#" class="header-button2 py-2 px-4 text-base font-medium text-indigo-600 hover:bg-indigo-50">Sign up</a>
       </div>
     </div>
   </nav>
