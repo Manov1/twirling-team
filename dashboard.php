b391df910432f0b3475f6864ab1a76e31d1b3808
AcAkb <566254@edu.rocmn.nl>
2022-10-14 12:02:27 +0200

    *

---
+++ dashboard.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="content-type" content="text/html">
+	<meta name="robots" content="noindex, nofollow">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js" crossorigin="anonymous"></script>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/dashboard.css">
+	<script>
+		if(window.history.replaceState) {
+			window.history.replaceState(null, null, window.location.href);
+		};
+	</script>
+</head>
+<body>
+	<div class="container-left">
+		<div class="button-wrapper">
+			<button class="button-style">Home</button>
+			<button class="button-style">Dashboard</button>
+			<button class="button-style">Overzicht</button>
+			<button class="button-style">Teams</button>
+			<button class="button-style">Leden</button>
+			<button class="button-style">Klanten</button>
+			<button class="button-style">Vacatures</button>
+		</div>
+		<div class="footer-wrapper">
+			<p>&copy; Copyright 2022 SL7.1</p>
+	</div>
+</div>
+</body>
+</html>
