064f6c46b07443eac67307c0f44a2f80ddaad024
AcAkb <566254@edu.rocmn.nl>
2022-10-14 11:41:04 +0200

    Dashboard & New Font

---
+++ dashboard/dashboard.php
@@ -0,0 +1,22 @@
+<script>
+	if(window.history.replaceState) {
+		window.history.replaceState(null, null, window.location.href);
+	};
+</script>
+<body>
+	<div class="container-left">
+		<div class="button-wrapper">
+			<button class="button-style">Home</button>
+			<button class="button-style">Dashboard</button>
+			<button class="button-style">Overzicht</button>
+			<button class="button-style">Teams</button>
+			<button class="button-style">Leden</button>
+			<button class="button-style">Klanten</button>
+			<button class="button-style">Vacatures</button>
+		</div>
+		<div class="footer-wrapper">
+			<p>&copy; Copyright 2022 SL7.1</p>
+		</div>
+	</div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -2,15 +2,19 @@
 <html lang="nl">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="content-type" content="text/html">
+    <meta name="robots" content="noindex, nofollow">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js" crossorigin="anonymous"></script>
     <title>[7.1] 🌀 Twirling Team Sparkle</title>
 
     <link rel="stylesheet" href="header/header.css">
     <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="dashboard/dashboard.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 <?php include 'header/header.php'; ?>
+<?php include 'dashboard/dashboard.php'; ?>
 </body>
 </html>
